ac1e1965da6484aa14ce3c379f81e02e3d3e5238
mnorouzi <mnorouzi@43aa559e-c8eb-47bc-a830-0eeea0e4f537>
2011-09-01 18:53:40 +0000

    correcting type definitions

--- trunk/utils/types.h
+++ trunk/utils/types.h
@@ -1,11 +1,13 @@
+#include <stdint.h>
+
 #ifndef TYPES_H__
 #define TYPES_H__
 
 // Types
-typedef unsigned long UINT64;
-typedef unsigned int UINT32;
-typedef unsigned short UINT16;
-typedef unsigned char UINT8;
+typedef uint64_t UINT64;
+typedef uint32_t UINT32;
+typedef uint16_t UINT16;
+typedef uint8_t UINT8;
 
 // Constants
 #define UINT64_1 ((UINT64)0x01)
